9debe5673e01f123c39faece0ae978ee93624a0d
dmcmaumau@gmail.com <dmcmaumau@gmail.com@82fddeee-f716-197e-29a0-3c1edd6c6528>
2013-10-08 19:32:57 +0000

    app cmr

--- trunk/appCMR/src/br/com/cmr/model/dao/ProducaoDAO.java
+++ trunk/appCMR/src/br/com/cmr/model/dao/ProducaoDAO.java
@@ -10,6 +10,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 /**
